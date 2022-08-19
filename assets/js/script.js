2f79863a2140bfb230757962c2b9bcb77032a29c
luannmoraess <luanmoraes10_@hotmail.com>
2022-08-19 20:23:25 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+function gerar(){
+        var a= prompt("qual sua renda ?")
+        var b= prompt("quanto quer em meses ?")
+        var soma= a * b
+        document.write(soma)
+
+        
+
+}
+
